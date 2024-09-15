f202a0b95cb5a5b842719cc33e6b72886d10ce5e
Tassadar2499 <34176883+Tassadar2499@users.noreply.github.com>
2024-09-15 22:07:22 +0300

    add pooled array

---
+++ CollectionsExtensions/PooledArray.cs
@@ -0,0 +1,36 @@
+﻿using System.Buffers;
+
+namespace CollectionsExtensions;
+
+public class PooledArray<T> : IDisposable
+{
+    public static PooledArray<T> RentFrom(int minimumLength)
+    {
+        var data = ArrayPool<T>.Shared.Rent(minimumLength);
+        
+        return new PooledArray<T>(data);
+    }
+    
+    public T[] Data { get; }
+
+    private PooledArray(T[] data)
+    {  
+        Data = data;
+    }
+
+    private void Return()
+    {
+        ArrayPool<T>.Shared.Return(Data);
+    }
+
+    ~PooledArray()
+    {
+        Return();
+    }
+
+    public void Dispose()
+    {
+        GC.SuppressFinalize(this);
+        Return();
+    }
+}

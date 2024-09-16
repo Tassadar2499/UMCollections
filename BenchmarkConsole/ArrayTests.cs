1e84be53a5f7588934a6e3cb85f918c74a3bd8c4
Tassadar2499 <34176883+Tassadar2499@users.noreply.github.com>
2024-09-16 22:51:50 +0300

    add benchmark

---
+++ BenchmarkConsole/ArrayTests.cs
@@ -0,0 +1,40 @@
+﻿using BenchmarkDotNet.Attributes;
+using CollectionsExtensions;
+
+namespace BenchmarkConsole;
+
+[MemoryDiagnoser]
+public class ArrayTests
+{
+    private const int PageSize = 8192;
+    private const int PageSizeRight = 81920;
+    private const int CycleCount = 1000;
+    
+    [Benchmark]
+    public void CheckDefaultArray()
+    {
+        for (int i = 0; i < CycleCount; i++)
+        {
+            var random = new Random();
+            var length = random.Next(PageSize, PageSizeRight);
+            var array = new int[length];
+            
+            for (int j = 0; j < length; j++)
+                array[j] = random.Next(0, 10);
+        }
+    }
+ 
+    [Benchmark]
+    public void CheckPooledArray()
+    {
+        for (int i = 0; i < CycleCount; i++)
+        {
+            var random = new Random();
+            var length = random.Next(PageSize, PageSizeRight);
+            using var array = PooledArray<int>.RentFrom(length);
+            
+            for (int j = 0; j < length; j++)
+                array.Data[j] = random.Next(0, 10);
+        }
+    }
+}
---
+++ BenchmarkConsole/Program.cs
@@ -0,0 +1,9 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using BenchmarkConsole;
+using BenchmarkDotNet.Configs;
+using BenchmarkDotNet.Running;
+
+var config = DefaultConfig.Instance;
+var summary = BenchmarkRunner.Run<ArrayTests>(config, args);
+Console.ReadKey();
